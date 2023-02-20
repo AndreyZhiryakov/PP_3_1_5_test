8cc24c2b780b95865b34bb37573ef6b13a9ab2d5
AndreyZhiryakov <aoj1976@yandex.ru>
2023-02-20 23:21:39 +0300

    ver_1
    20_02_23

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.entities.Role;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class UserServiceImp implements UserService {
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Autowired
+    UserRepository userRepository;
+//    @Autowired
+//    RoleRepository roleRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> userFromDb = userRepository.findById(userId);
+        return userFromDb.orElse(new User());
+    }
+
+    @Override
+    public List<User> getUsersList() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        User userFromDB = userRepository.findByUsername(user.getEmail());
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateUser(Long userId, User updateUser) {
+        User userToBeUpdate = userRepository.getById(userId);
+        userToBeUpdate.setFirstname(updateUser.getFirstname());
+        userToBeUpdate.setLastname(updateUser.getLastname());
+        userToBeUpdate.setAge(updateUser.getAge());
+        userToBeUpdate.setEmail(updateUser.getEmail());
+        userToBeUpdate.setPassword(bCryptPasswordEncoder.encode(updateUser.getPassword()));
+        userToBeUpdate.setRoles((List<Role>) updateUser.getRoles());
+        userRepository.save(userToBeUpdate);
+
+    }
+}
