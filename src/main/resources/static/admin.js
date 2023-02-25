f281c3dc67434f02615a6b1ebd8560d26ae3123f
AndreyZhiryakov <aoj1976@yandex.ru>
2023-02-25 19:20:37 +0300

    ver_6
    25_02_23

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,2 +1,16 @@
-alert('Hello.All is OK');
+ async function getAllUsers(){
+    const res = await fetch('api/admin');
+    const users = await res.json();
+    console.log(users);
+ }
+
+ window.addEventListener('DOMContentLoaded',getAllUsers);
+
+function usersToHTML({id,firstname,lastname,age,email,role}){
+    const usersList = document.getElementById('users')
+
+}
+
+
+
 
