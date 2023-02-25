c1f72f29f6f5fc6d9d6f1f8d2971c42b605cb0f9
AndreyZhiryakov <aoj1976@yandex.ru>
2023-02-26 02:03:51 +0300

    ver_7
    All users in admin-page

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,11 +1,30 @@
 fetch("api/admin").then((data) =>{
-   //console.log(data);
    return data.json();// converted to object
 }).then((objectData)=>{
-   console.log(objectData[0].firstname);
-   let tableDate= "";
+   let tableData = "";
    objectData.map((value)=>{
-    tableData=`${value.id}`
+    tableData+=`<tr style="background-color:rgba(58,74,74,0.06)" >
+
+                                    <td>${value.id}</td>
+                                    <td>${value.firstname}</td>
+                                    <td>${value.lastname}</td>
+                                    <td>${value.age}</td>
+                                    <td>${value.email}</td>
+                                    <td>${value.role}</td>
+                                </td>
+                                    <td>
+                                        <button type="button" class="btn btn-info" data-toggle="modal" role="dialog">
+                                            Edit
+                                        </button>
+                                    </td>
+                                    
+                                    <td>
+                                        <button type="button" class="btn btn-danger" data-toggle="modal"
+                                                role="dialog" >
+                                            Delete user
+                                        </button>
+                                    </td>
+                                </tr>`;
    });
    document.getElementById("users")
    .innerHTML = tableData;
