c95106e152a6032cf57a3df95c9df77b0053c6b0
AndreyZhiryakov <aoj1976@yandex.ru>
2023-03-02 00:27:34 +0300

    ver_6_1
    02_03_23

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -2,7 +2,7 @@ const url = "api/admin";
 const urlNew = "api/new";
 
 
-const usersTable  = document.getElementById("users");
+const usersTable = document.getElementById("users");
 const addUserForm = document.getElementById("new-user-form");
 const firstNameValue = document.getElementById('firstname-value');
 const lastNameValue = document.getElementById('lastname-value');
@@ -13,45 +13,9 @@ const rolesValue = document.getElementById('roles');
 let output = "";
 
 
-//Get all users
-// const usersCreate = (users) => {
-//     users.map((value) => {
-//         output += `
-// <tr>
-// <td>${value.id}</td>
-// <td>${value.firstname}</td>
-// <td>${value.lastname}</td>
-// <td>${value.age}</td>
-// <td>${value.email}</td>
-// <td>
-// <th:block>
-//         <span th:switch="">
-//         <span th:case="'ROLE_ADMIN'">ADMIN</span>
-//         <span th:case="'ROLE_USER'">USER</span>
-//         </span>
-// </th:block>
-// <td>
-// <button type="button" class="btn btn-info" data-toggle="modal" role="dialog"
-//                   data-target= "#editModal">
-//   Edit
-// </button>
-// </td>
-// <td>
-// <button type="button" class="btn btn-danger" data-toggle="modal"
-//        role="dialog" data-target="#deleteModal">
-//        Delete user
-//        </button>
-// </td>
-// </tr>`;
-//     });
-//     usersTable.innerHTML = output;
-// }
-
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      data.forEach(user => {
-          output += `
+const usersCreate = (users) => {
+    users.forEach(user => {
+        output += `
 <tr>
 <td>${user.id}</td>
 <td>${user.firstname}</td>
@@ -79,47 +43,15 @@ fetch(url)
 </td>
 </tr>`;
 
-      })
-        usersTable.innerHTML = output;
     })
+    usersTable.innerHTML = output;
+}
+fetch(url)
+    .then(res => res.json())
+    .then(data => usersCreate(data))
 
 
-//     .then(res => res.json())
-//     .then(data => {
-//
-//         output += `
-// <tr>
-// <td>${value.id}</td>
-// <td>${value.firstname}</td>
-// <td>${value.lastname}</td>
-// <td>${value.age}</td>
-// <td>${value.email}</td>
-// <td>
-// <th:block>
-//         <span th:switch="">
-//         <span th:case="'ROLE_ADMIN'">ADMIN</span>
-//         <span th:case="'ROLE_USER'">USER</span>
-//         </span>
-// </th:block>
-// <td>
-// <button type="button" class="btn btn-info" data-toggle="modal" role="dialog"
-//                   data-target= "#editModal">
-//   Edit
-// </button>
-// </td>
-// <td>
-// <button type="button" class="btn btn-danger" data-toggle="modal"
-//        role="dialog" data-target="#deleteModal">
-//        Delete user
-//        </button>
-// </td>
-// </tr>`;
-//     });
-// usersTable.innerHTML = output;
 
-
-//Create new user
-//POST method
 
 addUserForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -130,47 +62,18 @@ addUserForm.addEventListener('submit', (e) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            firstname:firstNameValue.value,
-            lastname:lastNameValue.value,
-            age:ageValue.value,
-            email:emailValue.value,
-            password:passwordValue.value,
-            roles_:rolesValue.value
-        })
-        })
-        // .then(res => res.json())
-        // .then(data =>
-        //     usersCreate(data))
+            firstname: firstNameValue.value,
+            lastname: lastNameValue.value,
+            age: ageValue.value,
+            email: emailValue.value,
+            password: passwordValue.value,
+            roles_: rolesValue.value
 
-
+        })
     })
+})
 
 
-// newUser.addEventListener('click', () => {
-//     $('#newModal').modal('show')
-// })
 
-// document.getElementById('addUserButton').addEventListener('click',
-//     async () => {
-//     const newFirstName = document.getElementById('newfirstname');
-//     const firstname = newFirstName.value;
-//     console.log(firstname);
-//     const newLastName = document.getElementById('newlastname');
-//     const lastname = newLastName.value;
-//     const newAge = document.getElementById('newage');
-//     const age = newAge.value;
-//     const newEmail = document.getElementById('newemail');
-//     const email = newEmail.value;
-//     const newPassword = document.getElementById('newpassword');
-//     const password = newPassword.value;
-//     const role = 'ROLE_ADMIN';
-//
-//     const res = await fetch("api/new", {
-//         method: 'POST',
-//         body: JSON.stringify({firstname,lastname,age,email,password,role})
-//     });
-//     const user = await res.json();
-//     console.log(user);
-//     })
 
 
