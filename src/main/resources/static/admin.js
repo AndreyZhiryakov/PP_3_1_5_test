22b29e5cc864a158af4f0baaa618e661c086ef5e
AndreyZhiryakov <aoj1976@yandex.ru>
2023-03-10 02:23:44 +0300

    Revert "ver_13"
    This reverts commit c697397f97246bafe8d1a966beec8a29f7389fa6.

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -3,7 +3,8 @@ const urlNew = "api/new";
 const urlDel = "api/delete"
 const urlEdit = "api/edit"
 
-const dbRoles = [{id: 1, role: "ROLE_USER"}, {id: 2, role: "ROLE_ADMIN"}]
+const dbRoles = [{id: 1, name: "ROLE_USER", authority: "ROLE_USER", unRole: "USER" }, {id: 2, name: "ROLE_ADMIN",
+    authority: "ROLE_ADMIN", unRole: "ADMIN"}]
 
 
 const btnNewUser = document.getElementById("new-btn")
@@ -168,38 +169,41 @@ usersTable.addEventListener('click', (e) => {
 addUserForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    // const select = document.getElementById('roles-value');
+    const select = document.getElementById('roles-value');
     // var options = select.selectedOptions;
-    //
-    // const values = Array.from(options).map(({ value }) => ({value}))
-    // // console.log(values);
-    // //var values =  Array.from(select.options).filter(option => option.selected).map(option => option.value);
-    //console.log("VALUES " , values);
-    //console.log("dbRoles" + dbRoles);
-    // var rolesUserArr = new Map();
-    // for (let x = 0; x < values.length; x++) {
-    //     //console.log("++++++++" + values.length)
-    //     for (let y = 0; y < dbRoles.length; y++) {
-    //         //console.log("---------" + values.length)
-    //         if (values[x] == dbRoles[y].role) {
-    //             //console.log("!!!!!!!!!" + values.length)
-    //             rolesUserArr.set(dbRoles[y].id, dbRoles[y].role);
-    //             console.log("dbRoles[y].id, dbRoles[y].role  " +  dbRoles[y].id, dbRoles[y].role);
-    //         }
-    //     }
-    // }
-    // console.log("rolesUserArr" + rolesUserArr);
-    //
-    // let listRoles = roleArray(document.getElementById('roles-value'))
-    //
-    // console.log("listRoles " + listRoles[0] + listRoles[1]);
-    // var roleSetString = "[";
-    // for (let pair of rolesUserArr) roleSetString +=`{"id":${pair[0]}, "role":"${pair[1]}"},`;
-    // roleSetString =  roleSetString.slice(0, -1);
-    // roleSetString += "]";
-    // var roleSet = JSON.parse(roleSetString);
-    //
-    // console.log("roleSet" + roleSet);
+    //var values = Array.from(options).map(({value}) => value);
+    // console.log(values);
+    var values =  Array.from(select.options).filter(option => option.selected).map(option => option.value);
+
+    console.log("VALUES " + values);
+    console.log("dbRoles" + dbRoles);
+
+    var rolesUserArr = new Map();
+    for (let x = 0; x < values.length; x++) {
+        //console.log("++++++++" + values.length)
+        for (let y = 0; y < dbRoles.length; y++) {
+            //console.log("---------" + values.length)
+            if (values[x] == dbRoles[y].name) {
+                //console.log("!!!!!!!!!" + values.length)
+                rolesUserArr.set(dbRoles[y].id, dbRoles[y].name);
+                //console.log(dbRoles[y].id, dbRoles[y].name);
+            }
+        }
+    }
+    console.log("rolesUserArr[0]" + rolesUserArr[0]);
+
+    let listRoles = roleArray(document.getElementById('roles-value'))
+
+    console.log("listRoles " + listRoles[0] + listRoles[1]);
+
+
+    var roleSetString = "[";
+    for (let pair of rolesUserArr) roleSetString +=`{"id":${pair[0]}, "name":"${pair[1]}","authority":"${pair[1]}", "unRole": "${pair[1].replace('ROLE_','')}"},`;
+    roleSetString =  roleSetString.slice(0, -1);
+    roleSetString += "]";
+    var roleSet = JSON.parse(roleSetString);
+
+    console.log("roleSet" + roleSet);
 
     fetch(urlNew, {
         method: 'POST',
@@ -214,24 +218,29 @@ addUserForm.addEventListener('submit', (e) => {
             password: passwordValue.value,
             roles:
                 [
-        {            id: 1,
-            role: "ROLE_USER"
+        {
+            id: 1,
+            role: "ROLE_USER",
+            authority: "ROLE_USER",
+            unRole: "USER"
         },
         {
             id: 2,
-            role: "ROLE_ADMIN"
+            role: "ROLE_ADMIN",
+            authority: "ROLE_ADMIN",
+            unRole: "ADMIN"
         }
     ]
         })
     })
         .then(res => res.json())
-        //.then(data => console.log(data))
-        .then(data => {
-            const dataArr = [];
-            dataArr.push(data);
-            usersCreate(dataArr);
-            location.reload();
-        })
+        .then(data => console.log(data))
+        // .then(data => {
+        //     const dataArr = [];
+        //     dataArr.push(data);
+        //     usersCreate(dataArr);
+        //     location.reload();
+        // })
 })
 
 let roleArray = (options) => {
