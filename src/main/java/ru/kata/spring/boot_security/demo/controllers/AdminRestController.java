92d1ab81d1ce2417badbbac43c02a9b566a37de2
AndreyZhiryakov <aoj1976@yandex.ru>
2023-03-15 00:25:06 +0300

    correction_3
    FIX 2) Внедри зависимости через конструктор.

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
@@ -1,7 +1,6 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
@@ -2,9 +2,7 @@ package ru.kata.spring.boot_security.demo.entities;
 
 
 import org.springframework.security.core.GrantedAuthority;
-
 import javax.persistence.*;
-
 
 @Entity
 @Table(name = "roles")
